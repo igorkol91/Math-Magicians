25950cce4ea493165f176513a111f12dcfd431dc
Igor.Koloski <koloski_igor@hotmail.com>
2021-09-13 22:21:39 +0200

    Fix style linter errors

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,7 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
+          {' '}
           <code>src/App.js</code>
+          {' '}
           and save to reload.
         </p>
         <a
