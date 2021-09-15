3cff998b90bdcb91b188156cd7529bdf8f37d742
Igor.Koloski <koloski_igor@hotmail.com>
2021-09-15 17:11:15 +0200

    Add class in App.js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div>
-      <Calculator />
-      {' '}
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Calculator />
+        {' '}
+      </div>
+    );
+  }
 }
 
 export default App;
