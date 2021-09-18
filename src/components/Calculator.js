ce1ee24fd02102449e8176d135e7f20c7964c078
Igor.Koloski <koloski_igor@hotmail.com>
2021-09-18 16:33:41 +0200

    Refactor code with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import Keyboard from './Keyboard';
 import calculate from './logic/calculate';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [state, setState] = useState({ total: null, next: null, operation: null });
+  const { next, total } = state;
 
-  handleClick = (btnName) => {
-    const newState = calculate(this.state, btnName);
-    this.setState((prevState) => ({
+  const handleClick = (btnName) => {
+    const newState = calculate(state, btnName);
+    setState((prevState) => ({
       ...prevState,
       ...newState,
     }));
   };
 
-  render() {
-    const { next, total } = this.state;
-    return (
-      <section className="bigContainer w-50">
-        <Display value={next || total || '0'} />
-        <Keyboard handle={this.handleClick} />
-      </section>
-    );
-  }
-}
+  return (
+    <section className="bigContainer w-50">
+      <Display value={next || total || '0'} />
+      <Keyboard handle={handleClick} />
+    </section>
+  );
+};
+
+export default Calculator;
